56c7018ba151c5b5ffc16aaa431c266e71ac0b87
DESKTOP-KTKC2M6\vmlf <vmlf.prudius@gmail.com>
2022-11-05 20:56:18 +0200

    Special controllers implemented

--- HormoneTracker/Controllers/ChartDataController.cs
+++ HormoneTracker/Controllers/ChartDataController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using HormoneTracker.BLL.Services;
+using HormoneTracker.Core.Models;
+using HormoneTracker.Core.Models.ServicesModels;
+using HormoneTracker.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HormoneTracker.Controllers
@@ -7,5 +12,37 @@ namespace HormoneTracker.Controllers
     [ApiController]
     public class ChartDataController : ControllerBase
     {
+        private readonly HormoneTrackerDBContext _context;
+        private readonly BasicChartDataService _chartService;
+
+        public ChartDataController(HormoneTrackerDBContext context,
+            BasicChartDataService chartService)
+        {
+            _context = context;
+            _chartService = chartService;
+        }
+
+        // GET: api/Charts/Analysis
+        [HttpGet]
+        [Authorize]
+        [Route("Analysis/{analysisId}")]
+        public ChartData GetAnalysisData(int analysisId)
+        {
+            Analysis analysis = _context.Analyses.Find(analysisId);
+
+            return _chartService.GetAnalysisData(analysis);
+        }
+
+        // GET: api/Charts/Analysis
+        [HttpGet]
+        [Authorize]
+        [Route("Patient/{patientId}/{analysisName}")]
+        public ChartData GetPatientAnalysisData(int patientId, string analysisName)
+        {
+            Patient patient = _context.Patients.Find(patientId);
+
+            return _chartService.GetPatientAnalysisData(patient, analysisName);
+        }
+
     }
 }
---
+++ HormoneTracker/Controllers/RecomendationsController.cs
@@ -0,0 +1,47 @@
+﻿using HormoneTracker.BLL.Services;
+using HormoneTracker.Core.Models;
+using HormoneTracker.Core.Models.ServicesModels;
+using HormoneTracker.DAL;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HormoneTracker.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecomendationsController : ControllerBase
+    {
+        private readonly HormoneTrackerDBContext _context;
+        private readonly BasicRecomendationsService _recomendationsService;
+
+        public RecomendationsController(HormoneTrackerDBContext context,
+            BasicRecomendationsService recomendationsService)
+        {
+            _context = context;
+            _recomendationsService = recomendationsService;
+        }
+
+        // GET: api/Recomendations/Analysis
+        [HttpGet]
+        [Authorize]
+        [Route("Patient/{patientId}")]
+        public List<Recomendation> GetPatientRecomendations(int patientId)
+        {
+            Patient patient = _context.Patients.Find(patientId);
+
+            return _recomendationsService.GetRecomendations(patient);
+        }
+
+        // GET: api/Recomendations/Analysis
+        [HttpGet]
+        [Authorize]
+        [Route("Analysis/{analysisId}")]
+        public List<Recomendation> GetPatientAnalysisData(int analysisId)
+        {
+            Analysis analysis = _context.Analyses.Find(analysisId);
+
+            return _recomendationsService.GetRecomendations(analysis);
+        }
+    }
+}
